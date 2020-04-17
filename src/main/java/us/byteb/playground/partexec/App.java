0adbd507434b685144d4258baf5cd710ea4c949a
Marc Löhe <marc@marcloehe.de>
2020-04-17 22:51:05 +0200

    feat: add grouping multithreaded executor

--- src/main/java/us/byteb/playground/partexec/App.java
+++ src/main/java/us/byteb/playground/partexec/App.java
@@ -1,16 +1,18 @@
 package us.byteb.playground.partexec;
 
 import us.byteb.playground.partexec.executor.Executor;
+import us.byteb.playground.partexec.executor.GroupingMultithreadedExecutor;
 import us.byteb.playground.partexec.executor.SyncExecutor;
 
 public class App {
 
-  private static final int NUM_BATCHES = 5;
-  private static final int NUM_SOURCES = 16;
+  private static final int NUM_BATCHES = 10;
   private static final int BATCH_SIZE = 100;
+  private static final int NUM_SOURCES = 16;
 
   public static void main(String[] args) throws InterruptedException {
     benchmark(SyncExecutor.class);
+    benchmark(GroupingMultithreadedExecutor.class);
   }
 
   private static void benchmark(final Class<? extends Executor> executorClass) {
@@ -30,11 +32,13 @@ public class App {
     final long endTime = System.nanoTime();
 
     if (!producer.getSourceStates().equals(consumer.getSourceStates())) {
+      System.out.println("Final producer state: " + producer.getSourceStates());
+      System.out.println("Final consumer state: " + consumer.getSourceStates());
       throw new IllegalStateException("Final producer and consumer states are not equal!");
     }
 
     final float durationMs = (endTime - startTime) / 1000000.0f;
-    System.out.printf("%s done in %.3f ms%n", executorClass.getSimpleName(), durationMs);
+    System.out.printf("%s done in %.3fms%n", executorClass.getSimpleName(), durationMs);
   }
 
 }
---
+++ src/main/java/us/byteb/playground/partexec/executor/GroupingMultithreadedExecutor.java
@@ -0,0 +1,53 @@
+package us.byteb.playground.partexec.executor;
+
+import us.byteb.playground.partexec.Message;
+import us.byteb.playground.partexec.MessageConsumer;
+import us.byteb.playground.partexec.MessageProcessor;
+import us.byteb.playground.partexec.MessageProducer;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Collectors;
+
+public class GroupingMultithreadedExecutor implements Executor {
+  final ExecutorService executorService = new ForkJoinPool(8);
+
+  public GroupingMultithreadedExecutor() {}
+
+  @Override
+  public void execute(
+      final MessageProducer producer,
+      final MessageProcessor processor,
+      final MessageConsumer consumer,
+      final int numBatches,
+      final int batchSize) {
+    for (int i = 0; i < numBatches; i++) {
+      final List<Message> messages = producer.nextBatch(batchSize);
+      executeBlockingWithOwnExecutorService(
+          () -> {
+            final Map<Integer, List<Message>> messagesBySource =
+                messages.stream().collect(Collectors.groupingBy(Message::getSource));
+
+            messagesBySource
+                .entrySet()
+                .parallelStream()
+                .map(Map.Entry::getValue)
+                .forEach(msgs -> consumer.consumeBatch(msgs, processor::process));
+          });
+      ;
+    }
+  }
+
+  private void executeBlockingWithOwnExecutorService(final Runnable runnable) {
+    try {
+      executorService.submit(runnable).get();
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException e) {
+      throw new IllegalStateException(e.getCause());
+    }
+  }
+}
